b22eca6ec1970bb3b41ddafe767aa682ad722918
rashidsikhani302-rg <rashidsikhani302@gmail.com>
2026-09-11 14:15:17 +0500

    Update token.example.js

--- token.example.js
+++ token.example.js
@@ -1,4 +1,4 @@
 // Copy to token.js locally and set your own bot token. Never commit token.js.
 module.exports = {
-  BOT_TOKEN: 'YOUR_TELEGRAM_BOT_TOKEN'
+  BOT_TOKEN: '8849150378:AAGW9ZcR-Sb3LbEtTTo8mXwvon8fp69LdrE'
 };
